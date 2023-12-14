30d614ffd2fd52e75d2543a316c5ff221aa014d5
yenaossai <yenaossai@gmail.com>
2023-12-14 21:08:56 +0000

    update

--- monty.h
+++ monty.h
@@ -53,6 +53,7 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+int main(int argc, char *argv[]);
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
